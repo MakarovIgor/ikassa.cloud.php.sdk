cfae2d324652b454b0388ff652344844f9db525b
Igormakarov1991 <igormakarov1991@gmail.com>
2025-05-13 15:50:51 +0300

    add fiscal document render

---
+++ src/RecieptRenderer/FiscalDocumentRender.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace igormakarov\IKassa\RecieptRenderer;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class FiscalDocumentRender
+{
+    private string $url;
+    private Client $httpClient;
+
+    public function __construct(string $environmentUrl)
+    {
+        $this->httpClient = new Client();
+        $this->url = $environmentUrl;
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function render(string $id): string
+    {
+        $response = $this->httpClient->request("GET", $this->url . "render/{$id}");
+        return $response->getBody()->getContents();
+    }
+}
